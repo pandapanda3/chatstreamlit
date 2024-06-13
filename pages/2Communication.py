ce7d3b011e3c62b9eaefd0195cd4221feba8ea7b
pandapanda3 <peggypanda30@gmail.com>
2024-06-13 20:57:30 +0100

    change communication

---
+++ pages/2Communication.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import streamlit as st
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+st.title("💬 Communication")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "patient", "content": "I have a headache."}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "dentist", "content": prompt})
+    st.chat_message("dentist").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "patient", "content": msg})
+    st.chat_message("patient").write(msg)
