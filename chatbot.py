ff243cba539e9161f1a15a4aa243b769447cb478
pandapanda3 <peggypanda30@gmail.com>
2024-06-13 21:19:29 +0100

    add login

--- chatbot.py
+++ chatbot.py
@@ -1,29 +1,34 @@
-from openai import OpenAI
+from st_pages import hide_pages
+from time import sleep
 import streamlit as st
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+def log_in():
+    st.session_state["logged_in"] = True
+    hide_pages([])
+    st.success("Logged in!")
+    sleep(0.5)
+    st.switch_page("pages/2Communication.py")
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+def log_out():
+    st.session_state["logged_in"] = False
+    st.success("Logged out!")
+    sleep(0.5)
 
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+
+if not st.session_state.get("logged_in", False):
+    hide_pages(["1Profile", "2Communication", "3History Conversation"])
+    username = st.text_input("Username", key="username")
+    password = st.text_input("Password", key="password", type="password")
+
+    if username == "test" and password == "test":
+        st.session_state["logged_in"] = True
+        hide_pages([])
+        st.success("Logged in!")
+        sleep(0.5)
+        st.switch_page("pages/2Communication.py")
+
+else:
+    st.write("Logged in!")
+    st.button("log out", on_click=log_out)
