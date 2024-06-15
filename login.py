e4fa096beeb7579c84b857ea6755d38cd50c94e8
pandapanda3 <peggypanda30@gmail.com>
2024-06-15 11:34:41 +0100

    modify the secrect data function

--- login.py
+++ login.py
@@ -2,20 +2,34 @@ import streamlit as st
 import pymysql
 from navigation import make_sidebar
 import bcrypt
+import boto3
 
-db_secrets = st.secrets["mysql"]
-
-
+# the data stores in .streamlit/secrets.toml, but it can't be exposed to github. So we need to have another way to approach it.
 def get_connection():
-    return pymysql.connect(
-        host=db_secrets["RDS_HOST"],
-        user=db_secrets["RDS_USER"],
-        password=db_secrets["RDS_PASSWORD"],
-        database=db_secrets["RDS_DB"],
-        port=db_secrets["RDS_PORT"],
-        charset=db_secrets["RDS_CHARTSET"]
-    )
+    # db_secrets = st.secrets["mysql"]
+    # return pymysql.connect(
+    #     host=db_secrets["RDS_HOST"],
+    #     user=db_secrets["RDS_USER"],
+    #     password=db_secrets["RDS_PASSWORD"],
+    #     database=db_secrets["RDS_DB"],
+    #     port=db_secrets["RDS_PORT"],
+    #     charset=db_secrets["RDS_CHARTSET"]
+    # )
+    return {
+        "RDS_HOST": get_ssm_parameter("RDS_HOST"),
+        "RDS_PORT": int(get_ssm_parameter("RDS_PORT")),
+        "RDS_DB": get_ssm_parameter("RDS_DB"),
+        "RDS_USER": get_ssm_parameter("RDS_USER"),
+        "RDS_PASSWORD": get_ssm_parameter("RDS_PASSWORD"),
+        "RDS_CHARSET": get_ssm_parameter("RDS_CHARSET")
+    }
 
+# store the secret data in aws
+def get_ssm_parameter(name):
+    ssm = boto3.client('ssm', region_name='London')
+    secret_data_whole = ssm.get_parameter(Name=name, WithDecryption=True)
+    return secret_data_whole['Parameter']['Value']
+    
 
 def authenticate_user(username, password):
     connection = get_connection()
