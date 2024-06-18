6f2e6a23b35dd776b94a5787cb839e647bc9d1d4
pandapanda3 <peggypanda30@gmail.com>
2024-06-18 21:45:16 +0100

    add suggestion table

--- pages/1Profile.py
+++ pages/1Profile.py
@@ -2,14 +2,34 @@ import streamlit as st
 from PIL import Image
 import io
 from navigation import make_sidebar
+from service.mysql import get_connection
 
+st.set_page_config(page_title="Profile", page_icon="👤")
 make_sidebar()
 
+def insert_suggestion(user, suggestion_content):
+    value = (user, suggestion_content)
+    connection = get_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO suggestions (user, suggestion_content) VALUES (%s, %s)"
+            cursor.execute(sql, value)
+            connection.commit()
+    finally:
+        connection.close()
+
 def main():
-    st.set_page_config(page_title="Profile", page_icon="👤")
-
+    
     st.title("Profile")
+    # Check if user is logged in
+    if 'logged_in' not in st.session_state or not st.session_state.logged_in:
+        st.warning("Please log in first.")
+        return
 
+    user_info = st.session_state.user_info
+    user = user_info['username']
+    
     # Profile Header
     st.markdown("<style>.header {text-align: center;}</style>", unsafe_allow_html=True)
 
@@ -27,7 +47,6 @@ def main():
         image = Image.open(uploaded_file)
         img_byte_arr = io.BytesIO()
         image.save(img_byte_arr, format='PNG')
-        img_byte_arr = img_byte_arr.getvalue()
 
         # Display the uploaded image
         st.image(image, caption='Uploaded Image.', use_column_width=True)
@@ -47,8 +66,13 @@ def main():
     # Suggestion Form
     st.markdown("### Feedback and Suggestion")
     suggestion = st.text_area("Write your suggestion here...")
+    if "suggestions" not in st.session_state:
+        st.session_state.suggestions = []
     if st.button("Submit"):
         if suggestion.strip():
+            suggestion_content = suggestion.strip()
+            # Insert into database
+            insert_suggestion(user, suggestion_content)
             st.success("Suggestion submitted successfully!")
         else:
             st.warning("Please write a suggestion before submitting.")
