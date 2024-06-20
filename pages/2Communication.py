6aa369f1730584ed72f6ae9519eff0635e81cdf9
pandapanda3 <peggypanda30@gmail.com>
2024-06-20 21:11:54 +0100

    add session id

--- pages/2Communication.py
+++ pages/2Communication.py
@@ -6,7 +6,6 @@ from service.mysql import get_connection, get_secret
 
 # insert message
 def insert_message(session_id, user_id, message, user_role):
-    
     value = (session_id, user_id, message, user_role)
     connection = get_connection()
     
@@ -15,7 +14,7 @@ def insert_message(session_id, user_id, message, user_role):
             sql = "INSERT INTO chat_records (session_id, user_id, message, user_role) VALUES (%s, %s, %s, %s)"
             cursor.execute(sql, value)
             connection.commit()
-            
+    
     finally:
         connection.close()
 
@@ -37,20 +36,22 @@ def get_largest_chat_number(user_id):
     finally:
         connection.close()
 
+
 # insert user_chat_history
-def insert_user_chat_history(user_id, user_name, chat_count, patient_details):
-    value = (user_id, user_name, chat_count, patient_details)
+def insert_user_chat_history(user_id, user_name, chat_count, patient_details, session_id):
+    value = (user_id, user_name, chat_count, patient_details, session_id)
     connection = get_connection()
     
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO user_chat_history (user_id, user_name, chat_count, patient_details) VALUES (%s, %s, %s, %s)"
+            sql = "INSERT INTO user_chat_history (user_id, user_name, chat_count, patient_details, session_id) VALUES (%s, %s, %s, %s, %s)"
             cursor.execute(sql, value)
             connection.commit()
     
     finally:
         connection.close()
-        
+
+
 # generate session id
 def generate_session_id():
     connection = get_connection()
@@ -81,13 +82,13 @@ user_role = current_user["role"]
 username = current_user["username"]
 print(f'st.session_state is :{st.session_state}')
 
+if "messages" not in st.session_state or not st.session_state["messages"]:
+    st.session_state["messages"] = [{"role": "patient", "content": "Hello, doctor. How are you today?"}]
+
 if 'session_id' not in st.session_state or st.session_state['session_id'] is None:
     st.session_state['session_id'] = generate_session_id()
 
 session_id = st.session_state['session_id']
-
-if "messages" not in st.session_state or not st.session_state["messages"]:
-    st.session_state["messages"] = [{"role": "patient", "content": "Hello, doctor. How are you today?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -99,21 +100,19 @@ if dentist_input := st.chat_input():
     # Check if there is only one message in the session state
     if 'messages' in st.session_state and len(st.session_state.messages) == 1:
         max_chat_count = get_largest_chat_number(user_id)
-        insert_user_chat_history(user_id, username, max_chat_count + 1, f'patient for {username}')
-
+        insert_user_chat_history(user_id, username, max_chat_count + 1, f'patient for {username}', session_id)
+    
     st.session_state.messages.append({"role": "dentist", "content": dentist_input})
     # show the message in the streamlit
     st.chat_message("dentist").write(dentist_input)
     # save the conversation
     insert_message(session_id, user_id, dentist_input, "dentist")
     
-    
     context = "\n".join([msg["content"] for msg in st.session_state.messages if msg["role"] == "dentist"])
-    patient_response = generate_patient_conversation(dentist_input=dentist_input, context=context, openai_api_key=openai_api_key)
-
+    patient_response = generate_patient_conversation(dentist_input=dentist_input, context=context,
+                                                     openai_api_key=openai_api_key)
+    
     st.session_state.messages.append({"role": "patient", "content": patient_response})
     # show the message in the streamlit
     st.chat_message("patient").write(patient_response)
     insert_message(session_id, user_id, patient_response, "patient")
-
-
