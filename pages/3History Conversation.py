22b75f300de963bb88e44d51d7e2458c87ccd78a
pandapanda3 <peggypanda30@gmail.com>
2024-06-20 20:42:54 +0100

    debug

--- pages/3History Conversation.py
+++ pages/3History Conversation.py
@@ -3,8 +3,6 @@ from navigation import make_sidebar
 from service.mysql import get_connection
 import pandas as pd
 
-
-st.set_page_config(layout="wide")
 make_sidebar()
 
 # Streamlit App
@@ -30,27 +28,36 @@ chat_history_data = fetch_chat_history_data(user_id)
 chat_history_data_df = pd.DataFrame(chat_history_data)
 print(f'data is :{chat_history_data_df}')
 
-
+data_df = pd.DataFrame(
+    {
+        "widgets": ["AA", "BB", "CC", "DD"],
+    }
+)
 
 # Display the data in Streamlit
 st.data_editor(
-    chat_history_data_df,
+    data_df,
     column_config={
-        "user_name": st.column_config.TextColumn(
+        "widgets": st.column_config.TextColumn(
             "User Name",
             help="The name of the user",
             width="medium"
         ),
-        "chat_count": st.column_config.NumberColumn(
-            "The number of question",
-            help="The number of chats",
-            width="medium"
-        ),
-        "patient_details": st.column_config.TextColumn(
-            "Patient Details",
-            help="Details of the patient",
-            width="large"
-        ),
+        # "user_name": st.column_config.TextColumn(
+        #     "User Name",
+        #     help="The name of the user",
+        #     width="medium"
+        # ),
+        # "chat_count": st.column_config.NumberColumn(
+        #     "The number of question",
+        #     help="The number of chats",
+        #     width="medium"
+        # ),
+        # "patient_details": st.column_config.TextColumn(
+        #     "Patient Details",
+        #     help="Details of the patient",
+        #     width="large"
+        # ),
     },
     hide_index=True,
 )
