2cf9b9fd016f60a3ebda598e68301c009dcbb214
pandapanda3 <peggypanda30@gmail.com>
2024-06-21 17:28:12 +0100

    get session id

--- pages/3History Conversation.py
+++ pages/3History Conversation.py
@@ -32,35 +32,35 @@ chat_history_data_df = pd.DataFrame(chat_history_data,
 # print(f'{type(chat_history_data_df)}, data df is :{chat_history_data_df}')
 # print(f'{type(chat_history_data)}, data is :{chat_history_data}')
 #
-# # Display the data in Streamlit
-# st.data_editor(
-#     chat_history_data_df,
-#     column_config={
-#         "user_name": st.column_config.TextColumn(
-#             "User Name",
-#             help="The name of the user",
-#             width="medium"
-#         ),
-#         "chat_count": st.column_config.NumberColumn(
-#             "The number of question",
-#             help="The number of chats",
-#             width="medium"
-#         ),
-#         "patient_details": st.column_config.TextColumn(
-#             "Patient Details",
-#             help="Details of the patient",
-#             width="large"
-#         ),
-#         "session_id": st.column_config.LinkColumn(
-#             "Session",
-#             help="Details of each chat detail",
-#             width="medium",
-#             display_text="session_id",
-#             url="history_page?session_id={session_id}",
-#         ),
-#     },
-#     hide_index=True,
-# )
+# Display the data in Streamlit
+st.data_editor(
+    chat_history_data_df,
+    column_config={
+        "chat_count": st.column_config.NumberColumn(
+            "The number of question",
+            help="The number of chats",
+            width="medium"
+        ),
+        "user_name": st.column_config.TextColumn(
+            "User Name",
+            help="The name of the user",
+            width="medium"
+        ),
+        "patient_details": st.column_config.TextColumn(
+            "Patient Details",
+            help="Details of the patient",
+            width="large"
+        ),
+        "session_id": st.column_config.LinkColumn(
+            "Session",
+            help="Details of each chat detail",
+            width="large",
+            display_text="click me to show the detail",
+            url="history_page?session_id={session_id}",
+        ),
+    },
+    hide_index=True,
+)
 
 # st.dataframe(chat_history_data_df)
 #
@@ -70,12 +70,19 @@ chat_history_data_df = pd.DataFrame(chat_history_data,
 #         st.query_params(session_id=session_id)
 #         st.rerun()
 
-st.write(chat_history_data_df)
-selected_row = st.selectbox("Select a row to get session_id", chat_history_data_df.index)
+# st.write(chat_history_data_df)
+# # selected_row = st.selectbox("Select a row to get session_id", chat_history_data_df.columns)
+# #
+# # # 提取选中行的 session_id
+# # session_id = chat_history_data_df.loc[selected_row, "session_id"]
+# #
+# # # 显示 session_id
+# # st.write(f"Selected session_id: {session_id}")
+# # st.query_params.session_id = session_id
 
-# 提取选中行的 session_id
-session_id = chat_history_data_df.loc[selected_row, "session_id"]
+session_ids = chat_history_data_df["session_id"].tolist()
 
-# 显示 session_id
-st.write(f"Selected session_id: {session_id}")
-st.query_params.session_id = session_id
+for session_id in session_ids:
+    if st.button(f"click me to jump into the detail: {session_id}", key=session_id):
+        st.query_params.session_id = session_id
+        st.page_link("pages/4History Detail Conversation.py", label="History Detail Conversation")
