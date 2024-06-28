dabfc7d6f2fbf7d66336707a3f0ed1fee522ee69
pandapanda3 <peggypanda30@gmail.com>
2024-06-28 20:31:11 +0100

    add quality

--- pages/2Communication.py
+++ pages/2Communication.py
@@ -1,74 +1,9 @@
 import streamlit as st
 from navigation import make_sidebar
 from service.generate_conversation import generate_patient_conversation, generate_patient_Symptoms
-from service.information_from_mysql import get_patient_symptoms_detail
-from service.mysql import get_connection, get_secret
-
-
-# insert message
-def insert_message(session_id, user_id, message, user_role):
-    value = (session_id, user_id, message, user_role)
-    connection = get_connection()
-    
-    try:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO chat_records (session_id, user_id, message, user_role) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, value)
-            connection.commit()
-    
-    finally:
-        connection.close()
-
-
-# get the largest number of chat_count of the user
-def get_largest_chat_number(user_id):
-    value = (user_id)
-    connection = get_connection()
-    
-    try:
-        with connection.cursor() as cursor:
-            sql = "SELECT MAX(chat_count) AS max_chat_count FROM user_chat_history WHERE user_id = %s"
-            cursor.execute(sql, value)
-            result = cursor.fetchone()
-            if result and len(result) > 0:
-                max_chat_count = result[0] if result[0] is not None else 0
-            else:
-                max_chat_count = 0
-            return max_chat_count
-    
-    finally:
-        connection.close()
-
-
-# insert user_chat_history
-def insert_user_chat_history(user_id, user_name, chat_count, patient_details, session_id):
-    value = (user_id, user_name, chat_count, patient_details, session_id)
-    connection = get_connection()
-    
-    try:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO user_chat_history (user_id, user_name, chat_count, patient_details, session_id) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(sql, value)
-            connection.commit()
-    
-    finally:
-        connection.close()
-
-
-# generate session id
-def generate_session_id():
-    connection = get_connection()
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT MAX(session_id) FROM chat_records")
-            result = cursor.fetchone()
-            if result and len(result) > 0:
-                max_session_id = result[0] if result[0] is not None else 0
-            else:
-                max_session_id = 0
-            return max_session_id + 1
-    finally:
-        connection.close()
+from service.information_from_mysql import generate_session_id, get_largest_chat_number, \
+    insert_user_chat_history, insert_message, update_user_chat_history_quality, update_quality_of_each_message
+from service.mysql import get_secret
 
 make_sidebar()
 
@@ -95,6 +30,17 @@ if 'session_id' not in st.session_state or st.session_state['session_id'] is Non
     st.session_state['session_id'] = generate_session_id()
 if 'patient_symptoms' not in st.session_state:
     st.session_state['patient_symptoms'] = ''
+if 'message_id' not in st.session_state:
+    st.session_state['message_id'] = 0
+def increment_message_id():
+    st.session_state['message_id'] += 1
+    return st.session_state['message_id']
+
+# get the largest chat number of a person
+max_chat_count = get_largest_chat_number(user_id)
+if not max_chat_count:
+    max_chat_count = 0
+chat_count = max_chat_count + 1
 
 session_id = st.session_state['session_id']
 
@@ -107,23 +53,22 @@ if dentist_input := st.chat_input():
         st.stop()
     # Check if there is only one message in the session state
     if 'messages' in st.session_state and len(st.session_state.messages) == 1:
-        max_chat_count = get_largest_chat_number(user_id)
         
-        if not max_chat_count:
-            max_chat_count = 0
-            
         # generate the symptoms of patient
         patient_Symptoms = generate_patient_Symptoms(openai_api_key)
         st.session_state['patient_symptoms'] = patient_Symptoms
         
-        insert_user_chat_history(user_id, username, max_chat_count + 1, patient_Symptoms, session_id)
+        insert_user_chat_history(user_id, username, chat_count, patient_Symptoms, session_id)
 
         
     st.session_state.messages.append({"role": "dentist", "content": dentist_input})
     # show the message in the streamlit
     st.chat_message("dentist").write(dentist_input)
+    
+    
     # save the conversation
-    insert_message(session_id, user_id, dentist_input, "dentist")
+    message_id = increment_message_id()
+    insert_message(session_id, user_id, dentist_input, "dentist", message_id)
     
     context = "\n".join([msg["content"] for msg in st.session_state.messages if msg["role"] == "dentist"])
     patient_response = generate_patient_conversation(dentist_input=dentist_input, context=context,
@@ -132,7 +77,15 @@ if dentist_input := st.chat_input():
     st.session_state.messages.append({"role": "patient", "content": patient_response})
     # show the message in the streamlit
     st.chat_message("patient").write(patient_response)
-    insert_message(session_id, user_id, patient_response, "patient")
+
+    message_id = increment_message_id()
+    insert_message(session_id, user_id, patient_response, "patient", message_id)
+    if st.button('👍', key='like_button'):
+        update_quality_of_each_message(session_id, user_id, message_id, 'good')
+    if st.button('👎', key='dislike_button'):
+        update_quality_of_each_message(session_id, user_id, message_id, 'bad')
+    
+    
 
 # if it has already generate the patient_information, show it in the sidebar
 if session_id:
@@ -140,3 +93,6 @@ if session_id:
     with st.sidebar:
         formatted_symptoms=st.session_state['patient_symptoms'].replace('\n', '<br>')
         st.markdown(formatted_symptoms, unsafe_allow_html=True)
+        conversation_score = st.number_input("How would you rate the quality of this conversation?", value=None, placeholder="Please enter a number between 0 and 100.")
+        st.write("The quality of this conversation is ", conversation_score)
+        update_user_chat_history_quality(user_id, username, chat_count, conversation_score)
--- service/information_from_mysql.py
+++ service/information_from_mysql.py
@@ -33,3 +33,97 @@ def get_patient_symptoms_detail(session_id):
     
     finally:
         connection.close()
+
+
+# insert chat_records of message and user_role
+def insert_message(session_id, user_id, message, user_role, message_id):
+    value = (session_id, user_id, message, user_role, message_id)
+    connection = get_connection()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO chat_records (session_id, user_id, message, user_role, message_id) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, value)
+            connection.commit()
+    
+    finally:
+        connection.close()
+
+
+# update the quality of each message in chat_records
+def update_quality_of_each_message(session_id, user_id, message_id, quality):
+    value = (quality, session_id, user_id, message_id)
+    connection = get_connection()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "UPDATE chat_records SET quality = %s WHERE session_id = %s AND user_id = %s AND message_id = %s"
+            cursor.execute(sql, value)
+            connection.commit()
+    
+    finally:
+        connection.close()
+        
+# get the largest number of chat_count of the user
+def get_largest_chat_number(user_id):
+    value = (user_id)
+    connection = get_connection()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT MAX(chat_count) AS max_chat_count FROM user_chat_history WHERE user_id = %s"
+            cursor.execute(sql, value)
+            result = cursor.fetchone()
+            if result and len(result) > 0:
+                max_chat_count = result[0] if result[0] is not None else 0
+            else:
+                max_chat_count = 0
+            return max_chat_count
+    
+    finally:
+        connection.close()
+
+
+# insert user_chat_history
+def insert_user_chat_history(user_id, user_name, chat_count, patient_details, session_id):
+    value = (user_id, user_name, chat_count, patient_details, session_id)
+    connection = get_connection()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO user_chat_history (user_id, user_name, chat_count, patient_details, session_id) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, value)
+            connection.commit()
+    
+    finally:
+        connection.close()
+
+
+# update the quality of user_chat_history
+def update_user_chat_history_quality(user_id, user_name, chat_count, conversation_score):
+    value = (conversation_score, user_id, user_name, chat_count)
+    connection = get_connection()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "UPDATE user_chat_history SET conversation_score = %s WHERE user_id = %s AND user_name = %s AND chat_count = %s"
+            cursor.execute(sql, value)
+            connection.commit()
+    
+    finally:
+        connection.close()
+
+# generate session id
+def generate_session_id():
+    connection = get_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT MAX(session_id) FROM chat_records")
+            result = cursor.fetchone()
+            if result and len(result) > 0:
+                max_session_id = result[0] if result[0] is not None else 0
+            else:
+                max_session_id = 0
+            return max_session_id + 1
+    finally:
+        connection.close()
