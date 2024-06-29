027fee45258ec5e247775e93389d108e64608053
pandapanda3 <peggypanda30@gmail.com>
2024-06-29 18:16:44 +0100

    add toggle

--- pages/2Communication.py
+++ pages/2Communication.py
@@ -78,15 +78,18 @@ if dentist_input := st.chat_input():
 
     message_id = increment_message_id()
     insert_message(session_id, user_id, patient_response, "patient", message_id)
+    # mark the performance
     col1, col2 = st.columns([1, 1])
     with col1:
-        if st.button(f'👍'):
+        good_on = st.toggle("Performance is good",key = 'good:'+str(message_id) )
+        if good_on:
             print(f'The message is good: session_id: {session_id}, user_id:{user_id}, message_id:{message_id}')
             update_quality_of_each_message(session_id, user_id, message_id, 'good')
-        with col2:
-            if st.button(f'👎'):
-                print(f'The message is bad')
-                update_quality_of_each_message(session_id, user_id, message_id, 'bad')
+    with col2:
+        bad_on = st.toggle("Performance is bad", key='bad:' + str(message_id))
+        if bad_on:
+            print(f'The message is bad')
+            update_quality_of_each_message(session_id, user_id, message_id, 'bad')
                 
 # if it has already generate the patient_information, show it in the sidebar
 if len(st.session_state.messages) > 1:
