b6a516ebd70b86f64d79e948b381ffd3a2abf59c
pandapanda3 <peggypanda30@gmail.com>
2024-07-08 21:37:10 +0100

    use aggrid

--- pages/3History Conversation.py
+++ pages/3History Conversation.py
@@ -2,6 +2,8 @@ import streamlit as st
 from navigation import make_sidebar
 from service.mysql import get_connection
 import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
+
 
 st.set_page_config(page_title="History Conversation", layout="wide")
 make_sidebar()
@@ -29,46 +31,52 @@ chat_history_data = fetch_chat_history_data(user_id)
 
 chat_history_data_df = pd.DataFrame(chat_history_data,
                                     columns=["chat_count", "user_name", "patient_details", "session_id"])
+#
+#
+# col1, col2 = st.columns([3, 1])
+#
+# with col1:
+#
+#     # Display the data in Streamlit
+#     st.data_editor(
+#         chat_history_data_df,
+#         column_config={
+#             "chat_count": st.column_config.NumberColumn(
+#                 "The number of chat session",
+#                 help="The number of chats",
+#                 width="small"
+#             ),
+#             "user_name": st.column_config.TextColumn(
+#                 "User Name",
+#                 help="The name of the user",
+#                 width="medium"
+#             ),
+#             "patient_details": st.column_config.TextColumn(
+#                 "Patient Details",
+#                 help="Details of the patient",
+#                 width="large"
+#             ),
+#             "session_id": st.column_config.NumberColumn(
+#                 "Tech Details (IGNORE IT)",
+#                 help="Tech Details (forget about it)",
+#                 width="small"
+#             ),
+#         },
+#         hide_index=True,
+#     )
+#
+# with col2:
+#     # when click the button, jump to another page to get the detail message
+#     for index, row in chat_history_data_df.iterrows():
+#         session_id = row["session_id"]
+#         chat_count = row["chat_count"]
+#         if st.button(f"Click me to jump into chat session: {chat_count}", key=chat_count):
+#             st.session_state.session_id = session_id
+#             st.session_state.chat_count = chat_count
+#             st.switch_page("pages/4History Detail Conversation.py")
+gb = GridOptionsBuilder.from_dataframe(chat_history_data_df)
+gb.configure_pagination(paginationAutoPageSize=True)  # 启用分页
+gridOptions = gb.build()
 
-
-col1, col2 = st.columns([3, 1])
-
-with col1:
-
-    # Display the data in Streamlit
-    st.data_editor(
-        chat_history_data_df,
-        column_config={
-            "chat_count": st.column_config.NumberColumn(
-                "The number of chat session",
-                help="The number of chats",
-                width="small"
-            ),
-            "user_name": st.column_config.TextColumn(
-                "User Name",
-                help="The name of the user",
-                width="medium"
-            ),
-            "patient_details": st.column_config.TextColumn(
-                "Patient Details",
-                help="Details of the patient",
-                width="large"
-            ),
-            "session_id": st.column_config.NumberColumn(
-                "Tech Details (IGNORE IT)",
-                help="Tech Details (forget about it)",
-                width="small"
-            ),
-        },
-        hide_index=True,
-    )
-
-with col2:
-    # when click the button, jump to another page to get the detail message
-    for index, row in chat_history_data_df.iterrows():
-        session_id = row["session_id"]
-        chat_count = row["chat_count"]
-        if st.button(f"Click me to jump into chat session: {chat_count}", key=chat_count):
-            st.session_state.session_id = session_id
-            st.session_state.chat_count = chat_count
-            st.switch_page("pages/4History Detail Conversation.py")
+# 显示 AgGrid
+AgGrid(chat_history_data_df, gridOptions=gridOptions)
