988f9f109826e532afa2d7265e4cf0e6bbb7c079
pandapanda3 <peggypanda30@gmail.com>
2024-07-09 11:55:21 +0100

    change link to button

--- pages/3History Conversation.py
+++ pages/3History Conversation.py
@@ -2,10 +2,8 @@ import streamlit as st
 from navigation import make_sidebar
 from service.mysql import get_connection
 import pandas as pd
-from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import JsCode
-from src.agstyler import draw_grid, get_numeric_style_with_precision, highlight
-from src.utils import get_screen_height
+from src.agstyler import draw_grid, get_numeric_style_with_precision
 
 
 st.set_page_config(page_title="History Conversation", layout="wide")
@@ -41,8 +39,15 @@ chat_history_data = fetch_chat_history_data(user_id)
 
 chat_history_data_df = pd.DataFrame(chat_history_data,
                                     columns=["chat_count", "user_name", "patient_details", "conversation_score", "session_id"])
-# Add link column to DataFrame
-chat_history_data_df['link'] = chat_history_data_df.apply(lambda row: generate_link(row['session_id'], row['chat_count']), axis=1)
+# Function to generate link button
+def generate_link_button(session_id, chat_count):
+    if st.button(f"Click to view session {chat_count}", key=f"{session_id}-{chat_count}"):
+        st.session_state.session_id = session_id
+        st.session_state.chat_count = chat_count
+        st.switch_page("pages/4History Detail Conversation.py")
+
+# Add link button column to DataFrame
+chat_history_data_df['link'] = chat_history_data_df.apply(lambda row: generate_link_button(row['session_id'], row['chat_count']), axis=1)
 
 formatter = {
     "chat_count": ("Chat Count", get_numeric_style_with_precision(0)),
