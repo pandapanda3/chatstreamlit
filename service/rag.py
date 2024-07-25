1d1697af2e90274dbf52db5ce9bfcb41d020447c
pandapanda3 <peggypanda30@gmail.com>
2024-07-25 16:38:53 +0100

    debuge

--- service/rag.py
+++ service/rag.py
@@ -18,7 +18,7 @@ def load_docx_from_dir(directory):
     return all_document_text
 
 def store_data(OPENAI_API_KEY):
-    document_direction = "service"
+    document_direction = "/home/ec2-user/chatstreamlit/src/document"
     print(f'当前文件：{document_direction}')
     document_texts = load_docx_from_dir(document_direction)
     
