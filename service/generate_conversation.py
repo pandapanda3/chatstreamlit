7e7c30585e4a75865341e0d2b02300432f9432d5
pandapanda3 <peggypanda30@gmail.com>
2025-01-09 19:56:52 +0000

    fix bug

--- service/generate_conversation.py
+++ service/generate_conversation.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 from langchain.chains import SequentialChain
 from langchain.chains import LLMChain
